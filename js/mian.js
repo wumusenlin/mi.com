02c8a5d44e799c67037ec827e9bf62c532ce8376
wumusenlin <49189384+wumusenlin@users.noreply.github.com>
2019-06-25 16:21:38 +0800

    js有问题需要修正

--- js/mian.js
+++ js/mian.js
@@ -3,21 +3,16 @@ var points = document.getElementsByClassName("point");
 var goPreBtn = document.getElementById("goPre");
 var goNextBtn = document.getElementById('goNext');
 var time = 30;
-var index = 0; 
+var	index = 0; 
 var leftul1 = document.getElementById('leftUl');
 var ull = document.querySelector('#shoujidianhua');
+
+
+
 // ull.click = function(){
 // 	leftul1.style.display = 'none';
 // 	console.log("asdfasdf")
 // }
-start();
-function start(){
-<<<<<<< HEAD
-	alert("zheshisdhfa");
-=======
-	alert("注意！这不是官网，是模仿小米商城的主页！");
->>>>>>> 29c3503c391b1ca77dca94cd5f99a95dea7f7b5c
-}
 function clearActive(){
 	for(var i = 0;i < items.length;i ++){
 		items[i].className =  'item';
